484714b163fdf7e5e0fac1ceece78b8e8c41149a
jabbaum <jabbaum@gmail.com>
2024-02-05 18:52:58 -0800

    gave the gameboard functions to add X and Y in specified locations

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // iife module pattern
 const Gameboard = (function(){
-    let gameboard=[];
+    let gameboard=[
+        0,1,2,
+        3,4,5,
+        6,7,8
+    ];
+    const PlaceX = (index) => gameboard[index] = 'X';
+    const PlaceY = (index) => gameboard[index] = 'Y';
+    const displayBoard = () => gameboard;
+
+    return {
+        PlaceX,
+        PlaceY,
+        displayBoard
+    }
 })();
+
+Gameboard.PlaceX(0);
+Gameboard.PlaceY(1);
+Gameboard.PlaceX(4);
+
+console.log(Gameboard.displayBoard());
