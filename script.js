da05ba83b5616c1b6b48183466c2d43a4f367882
jabbaum <jabbaum@gmail.com>
2024-03-02 11:19:17 -0800

    first pass at checking for winner

--- script.js
+++ script.js
@@ -5,19 +5,84 @@ const Gameboard = (function(){
         3,4,5,
         6,7,8
     ];
-    const PlaceX = (index) => gameboard[index] = 'X';
-    const PlaceY = (index) => gameboard[index] = 'Y';
-    const displayBoard = () => gameboard;
+    const Place = (index, symbol) => {
+        if(gameboard[index] !== 'X' && gameboard[index] !== 'Y') {
+            gameboard[index] = symbol
+            return checkWinner(index);
+        } else {
+            console.log(`Cell ${index} is taken, sorry.`);
+            return null;
+        }
+    };
+
+    const checkWinner = (index) => {
+        let winCondHorz = [0, 0, 0];
+        let winCondVert = [0, 0, 0];
+        let winCondDiag = [0, 0];
+        let currentSymbol = gameboard[index];
+        if(gameboard[0] === currentSymbol && gameboard[1] === currentSymbol && gameboard[2] === currentSymbol) {
+            winCondHorz[0] = 1;
+        } else if (gameboard[3] === currentSymbol && gameboard[4] === currentSymbol && gameboard[5] === currentSymbol) {
+            winCondHorz[1] = 1;
+        } else if (gameboard[6] === currentSymbol && gameboard[7] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondHorz[2] = 1;
+        } else if(gameboard[0] === currentSymbol && gameboard[3] === currentSymbol && gameboard[6] === currentSymbol) {
+            winCondVert[0] = 1;
+        } else if(gameboard[1] === currentSymbol && gameboard[4] === currentSymbol && gameboard[7] === currentSymbol) {
+            winCondVert[1] = 1;
+        } else if(gameboard[2] === currentSymbol && gameboard[5] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondVert[2] = 1;
+        } else if (gameboard[0] === currentSymbol && gameboard[4] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondDiag[0] = 1;
+        } else if (gameboard[2] === currentSymbol && gameboard[4] === currentSymbol && gameboard[6] === currentSymbol) {
+            winCondDiag[1] = 1;
+        };
+        return {
+            winCondVert,
+            winCondHorz,
+            winCondDiag
+        }
+    }
+
+    const DisplayBoard = () => gameboard;
 
     return {
-        PlaceX,
-        PlaceY,
-        displayBoard
+        Place,
+        DisplayBoard,
+        checkWinner
     }
 })();
 
-Gameboard.PlaceX(0);
-Gameboard.PlaceY(1);
-Gameboard.PlaceX(4);
+function createPlayer(name, symbol) {
+    return {name, symbol};
+}
+const Player1 = createPlayer('Player 1', 'X');
+const Player2 = createPlayer('Player 2', 'O');
 
-console.log(Gameboard.displayBoard());
+const GameLogic = (function(){
+    let winner = null;
+    let plays = [];
+    let rounds = 0;
+    let currentTurn = 1;
+
+    const IncrementRounds = () => rounds = rounds++;
+    const AddPlay = (play) => plays.push(play);
+    const changeTurn = (lastTurn) => {
+        if(lastTurn == 1) {
+        currentTurn = 2;
+        } else {
+            currentTurn = 1;
+        }
+    }
+    
+
+})();
+
+console.log(Gameboard.Place(0, 'X'));
+Gameboard.Place(1, 'Y');
+Gameboard.Place(4, 'X');
+console.log(Gameboard.Place(4, 'Y'));
+Gameboard.Place(2, 'Y');
+Gameboard.Place(8, 'X');
+
+console.log(Gameboard.DisplayBoard());
