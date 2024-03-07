7373a4d1df52fdf1fa467b832a00c7e9e7ea5521
jabbaum <jabbaum@gmail.com>
2024-03-06 20:23:53 -0800

    corrected accessing the adjusted row and column to access the returned items

--- script.js
+++ script.js
@@ -16,13 +16,23 @@ const Gameboard = (function(){
         [2, 1],
         [2, 2]
     ];
+
+    const AdjustLocation = (index) => {
+        const AdjustedRow = BoardLocator[index][0];
+        const AdjustedColumn = BoardLocator[index][1];
+        return {
+            AdjustedRow,
+            AdjustedColumn
+        }
+    }    
     const Place = (index, symbol) => {
-        const AdjustedIndex1 = BoardLocator[index][0];
-        const AdjustedIndex2 = BoardLocator[index][1];
+        const Adjusted = AdjustLocation(index);
+        const AdjustedRow = Adjusted.AdjustedRow;
+        const AdjustedColumn = Adjusted.AdjustedColumn;
         
-        if(gameboard[AdjustedIndex1][AdjustedIndex2] !== 'X' && gameboard[AdjustedIndex1][AdjustedIndex2] !== 'O') {
-            gameboard[AdjustedIndex1][AdjustedIndex2] = symbol
-            // CheckWinner(gameboard[AdjustedIndex1][AdjustedIndex2]);
+        if(gameboard[AdjustedRow][AdjustedColumn] !== 'X' && gameboard[AdjustedRow][AdjustedColumn] !== 'O') {
+            gameboard[AdjustedRow][AdjustedColumn] = symbol
+            // CheckWinner(index);
             currentPlay = [index, symbol];
             GameLogic.AddPlay(currentPlay)
             
@@ -34,35 +44,66 @@ const Gameboard = (function(){
         }
     };
 
-    // Need to convert to check the 2d array correctly
-    // const CheckWinner = (index) => {
-    //     let winCondHorz = [0, 0, 0];
-    //     let winCondVert = [0, 0, 0];
-    //     let winCondDiag = [0, 0];
-    //     let currentSymbol = gameboard[index];
-    //     if(gameboard[0] === currentSymbol && gameboard[1] === currentSymbol && gameboard[2] === currentSymbol) {
-    //         winCondHorz[0] = 1;
-    //     } else if (gameboard[3] === currentSymbol && gameboard[4] === currentSymbol && gameboard[5] === currentSymbol) {
-    //         winCondHorz[1] = 1;
-    //     } else if (gameboard[6] === currentSymbol && gameboard[7] === currentSymbol && gameboard[8] === currentSymbol) {
-    //         winCondHorz[2] = 1;
-    //     } else if(gameboard[0] === currentSymbol && gameboard[3] === currentSymbol && gameboard[6] === currentSymbol) {
-    //         winCondVert[0] = 1;
-    //     } else if(gameboard[1] === currentSymbol && gameboard[4] === currentSymbol && gameboard[7] === currentSymbol) {
-    //         winCondVert[1] = 1;
-    //     } else if(gameboard[2] === currentSymbol && gameboard[5] === currentSymbol && gameboard[8] === currentSymbol) {
-    //         winCondVert[2] = 1;
-    //     } else if (gameboard[0] === currentSymbol && gameboard[4] === currentSymbol && gameboard[8] === currentSymbol) {
-    //         winCondDiag[0] = 1;
-    //     } else if (gameboard[2] === currentSymbol && gameboard[4] === currentSymbol && gameboard[6] === currentSymbol) {
-    //         winCondDiag[1] = 1;
-    //     };
-    //     return {
-    //         winCondVert,
-    //         winCondHorz,
-    //         winCondDiag
-    //     }
-    // }
+    const CheckWinner = (index) => {
+        AdjustLocation(index);
+        let winCondHorz = [0, 0, 0];
+        let winCondVert = [0, 0, 0];
+        let winCondDiag = [0, 0];
+        let currentSymbol = gameboard[AdjustedRow][AdjustedColumn];
+
+        const CheckRow = (row) => {
+            let rowCells = ['empty', 'empty', 'empty'];
+            for (let index = 0; index < gameboard[row].length; index++) {
+                rowCells[index] = gameboard[row][index];
+            }
+            if (rowCells[0] === rowCells[1] && rowCells[0] === rowCells2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        const CheckColumn =  (column) => {
+            let columnCells = ['empty', 'empty', 'empty'];
+            for (let index = 0; index < gameboard.length; index++) {
+                columnCells[index] = gameboard[index][column]
+            }
+        }
+        const CheckDiagnol = (row, column) => {
+            const DiagTLBR = [[0,0], [1, 1], [2, 2]];
+            const DiagBLTR = [[2, 0], [1, 1], [0, 2]];
+            if ((row === 0 && column === 1) || (row === 2 && (column === 0 || column === 2)) || (row ===2 && column === 1)) {
+                return;
+            } else {
+                for (let index = 0; index < winCondDiag.length; index++) {
+                    
+                }
+            }
+        }
+
+
+        if(gameboard[0] === currentSymbol && gameboard[1] === currentSymbol && gameboard[2] === currentSymbol) {
+            winCondHorz[0] = 1;
+        } else if (gameboard[3] === currentSymbol && gameboard[4] === currentSymbol && gameboard[5] === currentSymbol) {
+            winCondHorz[1] = 1;
+        } else if (gameboard[6] === currentSymbol && gameboard[7] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondHorz[2] = 1;
+        } else if(gameboard[0] === currentSymbol && gameboard[3] === currentSymbol && gameboard[6] === currentSymbol) {
+            winCondVert[0] = 1;
+        } else if(gameboard[1] === currentSymbol && gameboard[4] === currentSymbol && gameboard[7] === currentSymbol) {
+            winCondVert[1] = 1;
+        } else if(gameboard[2] === currentSymbol && gameboard[5] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondVert[2] = 1;
+        } else if (gameboard[0] === currentSymbol && gameboard[4] === currentSymbol && gameboard[8] === currentSymbol) {
+            winCondDiag[0] = 1;
+        } else if (gameboard[2] === currentSymbol && gameboard[4] === currentSymbol && gameboard[6] === currentSymbol) {
+            winCondDiag[1] = 1;
+        };
+        return {
+            winCondVert,
+            winCondHorz,
+            winCondDiag
+        }
+    }
 
     const DisplayBoard = () => gameboard;
 
